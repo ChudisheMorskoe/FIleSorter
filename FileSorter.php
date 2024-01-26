14595ab52255d73dabff68505fb0a97d6ffa4093
chudishe <vitmanarina.v@gmail.com>
2024-01-26 14:22:34 +0200

    bug fixed

--- FileSorter.php
+++ FileSorter.php
@@ -2,23 +2,23 @@
 /**
  * @throws Exception
  */
-function getFilesDownloads($path = 'Downloads'): array
+
+function getFilesFromDirectory(string $path = 'Downloads'): array
 {
     $downloadsPath = $_SERVER['HOME'] . DIRECTORY_SEPARATOR . $path;
     if (!is_dir($downloadsPath)) {
         throw new Exception("The passed path $path is not valid");
     }
-
-    return searchFiles($downloadsPath);
+    return retrieveFiles($downloadsPath);
 }
 
 /**
  * @throws Exception
  */
-function searchFiles($directory): array
+function retrieveFiles(string $directory): array
 {
     $files = [];
-
+    $months = 6;
     $open = opendir($directory);
     if (!$open) {
         throw new Exception("Can't open dir: $directory");
@@ -29,14 +29,17 @@ function searchFiles($directory): array
             $filePath = $directory . DIRECTORY_SEPARATOR . $fileName;
 
             if (is_dir($filePath)) {
-                $files = array_merge($files, searchFiles($filePath));
+                $files = array_merge($files, retrieveFiles($filePath));
             } else {
-                $changeDateFormat = date('Y-m-d H:i:s', filemtime($filePath));
-                $files[] = [
-                    'name' => $fileName,
-                    'path' => $filePath,
-                    'changeDate' => $changeDateFormat,
-                ];
+                $lastAccessTime = fileatime($filePath);
+                $monthsAgo = strtotime(" -$months  months");
+                if ($lastAccessTime < $monthsAgo) {
+                    $files[] = [
+                        'name' => $fileName,
+                        'path' => $filePath,
+                        'lastAccessTime' => date('Y-m-d H:i:s', $lastAccessTime)
+                    ];
+                }
             }
         }
     }
@@ -45,69 +48,35 @@ function searchFiles($directory): array
     return $files;
 }
 
-function sortFiles(array $files, string $archivePath): void
+function archiveOldFiles(array $files, string $archivePath, int $months = 6): void
 {
-    foreach ($files as $file) {
-        $downloadsDirectory = $_SERVER['HOME'] . DIRECTORY_SEPARATOR . 'Downloads';
-
-        $pathInfo = pathinfo($file['name'], PATHINFO_EXTENSION);
-        $pathToFolder = $downloadsDirectory . DIRECTORY_SEPARATOR . $pathInfo;
-
-        if (!is_dir($pathToFolder)) {
-            mkdir($pathToFolder, 0755, true);
-        }
-
-        $newFilePath = $pathToFolder . DIRECTORY_SEPARATOR . $file['name'];
-
-        if (!strpos($file['path'], $archivePath) && !file_exists($newFilePath)) {
-            $absoluteOldPath = realpath($file['path']);
-            $absoluteNewPath = realpath($newFilePath);
-
-            if ($absoluteOldPath && $absoluteNewPath) {
-                rename($absoluteOldPath, $absoluteNewPath);
-            }
-        }
-    }
-}
-
-
-function archiveOldFiles($files): void
-{
-    $archivePath = $_SERVER['HOME'] . DIRECTORY_SEPARATOR . 'Downloads/ArchiveOldFiles';
-
-    if (!is_dir($archivePath)) {
-        mkdir($archivePath, 0755);
-    }
-
-    $sixMonths = 6;
+    $zipFileName = $archivePath . DIRECTORY_SEPARATOR . 'archive.zip';
     $zip = new ZipArchive();
+    $zip->open($zipFileName, ZipArchive::CREATE);
 
     foreach ($files as $file) {
-        $fileChangeDate = strtotime($file['changeDate']);
-        $sixMonthsAgo = strtotime("-$sixMonths months");
-
-        if ($fileChangeDate < $sixMonthsAgo) {
-            $newFilePathForArchive = $archivePath . DIRECTORY_SEPARATOR . $file['name'];
-
-            if (!file_exists($newFilePathForArchive)) {
-                rename($file['path'], $newFilePathForArchive);
-
-                $zipFileName = $archivePath . DIRECTORY_SEPARATOR . 'archive.zip';
-                $zip->open($zipFileName, ZipArchive::CREATE);
-                $zip->addFile($newFilePathForArchive, $file['name']);
-                $zip->close();
-
-                unlink($newFilePathForArchive);
+        $lastAccessTime = strtotime($file['lastAccessTime']);
+        $archiveThreshold = strtotime("-$months months");
+        if ($lastAccessTime < $archiveThreshold) {
+            if (!file_exists($archivePath . DIRECTORY_SEPARATOR . $file['name'])) {
+                $zip->addFile($file['path'], $file['name']);
             }
+        }
+    }
+    $zip->close();
+    foreach ($files as $file) {
+        $lastAccessTime = strtotime($file['lastAccessTime']);
+        $archiveThreshold = strtotime("-$months months");
+        if ($lastAccessTime < $archiveThreshold) {
+            unlink($file['path']);
         }
     }
 }
 
 try {
     $downloadsPath = $_SERVER['HOME'] . DIRECTORY_SEPARATOR . 'Downloads';
-    $filesInDownloads = getFilesDownloads();
-    archiveOldFiles($filesInDownloads);
-    sortFiles($filesInDownloads, $downloadsPath . DIRECTORY_SEPARATOR . 'ArchiveOldFiles');
+    $filesInDirectory = getFilesFromDirectory();
+    archiveOldFiles($filesInDirectory, $downloadsPath . DIRECTORY_SEPARATOR . 'ArchiveOldFiles');
 } catch (Exception $e) {
     echo 'Error: ' . $e->getMessage();
 }
